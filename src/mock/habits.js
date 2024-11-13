39c2a4afb685c51cc7975eed8dc49c31b2014135
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2024-11-13 14:36:35 +0300

    mock

---
+++ src/mock/habits.js
@@ -0,0 +1,16 @@
+const habits = [
+  {
+    id: 1,
+    name: 'Утренняя зарядка',
+    description: 'Разминка',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Бег по утрам',
+    description: 'Бег в 10:00',
+    status: 'completed',
+  },
+];
+
+export { habits };
